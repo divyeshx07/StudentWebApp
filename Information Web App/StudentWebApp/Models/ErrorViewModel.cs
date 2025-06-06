c6ce61c9a27bed92ab602e6b56a16bb1ba2b83cf
divyeshx07 <divyeshapatel03@gmail.com>
2025-06-06 15:29:06 +0530

    Add files via upload

---
+++ Information Web App/StudentWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
